766ede9797213772df165ced59938e690608c8fc
nahakritesh06 <nahakritesh06@gmail.com>
2026-02-06 23:48:05 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/bank/intent_check_svc/IntentCheckSvcApplication.java
@@ -0,0 +1,13 @@
+package com.bank.intent_check_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntentCheckSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntentCheckSvcApplication.class, args);
+	}
+
+}
